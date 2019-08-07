1110477cf403eb75e53d66f632a26846dfdb185b
Patrick Gordon <patrickgordon42@yahoo.com>
2019-08-07 16:50:51 -0500

    did calidation and error messages, made post request

--- user-onboarding/src/Components/OnboardForm.js
+++ user-onboarding/src/Components/OnboardForm.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import Axios from 'axios';
+import styled from 'styled-components'
+
+//styled components
+
+const OnboardFormStyle = styled.div`
+    width: 460px;
+    margin: auto 100px;
+    padding: 32px;
+    background-color: #fff;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)
+`
 
 
 
-const OnboardForm = ({ errors, touched, values}) => {
-    return(
-        <div>
+const OnboardForm = ({ errors, touched, values }) => {
+    return (
+        <OnboardFormStyle>
             <h1>Sign Up</h1>
             <Form>
                 <Field type='text' name='userName' placeholder="User Name" />
                 {touched.userName && errors.userName && (
                     <p>{errors.userName}</p>
                 )}
+
+                <Field type='text' name='email' placeholder="Email" />
+                {touched.email && errors.email && (
+                    <p>{errors.email}</p>
+                )}
+
+                <Field type='text' name='password' placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p>{errors.password}</p>
+                )}
+
+                <label>
+                    Terms Of Service
+                    <Field type='checkbox' name='termsOfService' checked={values.termsOfService} />
+                </label>
+                <button type='submit'>Submit!</button>
             </Form>
-        </div>
+        </OnboardFormStyle>
     );
 };
 
@@ -22,14 +50,28 @@ const OnboardForm = ({ errors, touched, values}) => {
 //HOC
 const FormikOnboardForm = withFormik({
     mapPropsToValues({ userName, email, password, termsOfService }) {
-        return{
+        return {
             userName: userName || '',
             email: email || '',
             password: password || '',
             termsOfService: termsOfService || false
         }
+    },
+
+    validationSchema: Yup.object().shape({
+        userName: Yup.string().required('Hey bro or broess, you need a user name!'),
+        email: Yup.string().required('Hey bro or broess, you need an email in here!'),
+        password: Yup.string(6).required('Hey bro or broess, you need a password here!'),
+        termsOfService: Yup.bool().oneOf([true], 'Hey man you have to check this box to move on')
+    }),
+
+    handleSubmit(values) {
+        Axios
+        .post('https://reqres.in/api/users', values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
     }
-})
+})(OnboardForm)
 
 
 
